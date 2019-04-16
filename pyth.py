e94e876daa7f8a82d8368b4756b7d6eb0431be67
VijayalakshmiHuddar <viju.oct10@gmail.com>
2019-04-16 08:36:29 +0530

    newpyfile

---
+++ pyth.py
@@ -0,0 +1,10 @@
+ Volume in drive C is OS
+ Volume Serial Number is 5082-62AD
+
+ Directory of C:\Users\Vijayalakshmi Huddar\Desktop\data_new
+
+16-04-2019  08:35    <DIR>          .
+16-04-2019  08:35    <DIR>          ..
+16-04-2019  08:35                 0 pyth.py
+               1 File(s)              0 bytes
+               2 Dir(s)  323,352,285,184 bytes free
